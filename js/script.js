89e71ba888a985cd3394e4cf1ed19fd1bef4b5b3
Katie <huang.katie94@gmail.com>
2014-05-31 18:01:28 -0700

    deleted some footers

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
      $('#homepage-logo-img').fadeIn(2000);
    });
 
+
+
 // Use 'jQuery(function($) {' for inside WordPress blogs (without quotes)
 $(document).ready(function () {
 	var open = false;
